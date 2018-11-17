9498fd5d834a54c35c11fda2de269d6a9b73df82
Steve OHearn <soh@corbinian.com>
2018-11-17 01:30:05 -0500

    Created new someStuff logic

--- src/test.py
+++ src/test.py
@@ -6,6 +6,17 @@ print("Start")
 
 theValue = "Hello there!"
 
+theQuery = "INSERT INTO"
+
+print(theQuery)
+
+someStuff = []
+someStuff.append(1)
+someStuff.append(2)
+
+for stuff in somestuff:
+    print(stuff)
+
 print(theValue)
 
 print("End")
