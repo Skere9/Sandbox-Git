985306c4cc5bbdb374dcbabab06db30a3d932405
Steve OHearn <soh@corbinian.com>
2018-11-17 01:09:10 -0500

    Added array logic

--- src/test.py
+++ src/test.py
@@ -2,6 +2,15 @@
 # Test 
 # 
 
+# This is from the thing I want to keep
+
 print("Start")
+
+theArray = []
+theArray.append({"name": "Joe", "dept": "Marketing"})
+theArray.append({"name": "Fred", "dept": "President"})
+
+for ary in theArray:
+    print(ary['name'])
 
 print("End")
